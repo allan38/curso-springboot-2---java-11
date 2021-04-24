f6a50885664457e9f81e9a215209d40d098a0cd3
allan38 <allanpeixoto38@gmail.com>
2021-04-24 14:02:04 -0300

    Camada de serviço - recupperando informação do banco

---
+++ src/main/java/com/allan/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.allan.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.allan.course.entities.User;
+import com.allan.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();	
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
