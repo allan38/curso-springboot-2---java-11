c73d155004e599da8587c8b18e6493ff8c8b6f9f
allan38 <allanpeixoto38@gmail.com>
2021-04-24 21:16:45 -0300

    Classe categoria

---
+++ src/main/java/com/allan/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.allan.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.allan.course.entities.Category;
+import com.allan.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();	
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
