2d3d14159ff221e1759b3115bb61de7548ab82b9
allan38 <allanpeixoto38@gmail.com>
2021-04-25 16:51:46 -0300

    Exceção do findByid

--- src/main/java/com/allan/course/services/UserService.java
+++ src/main/java/com/allan/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.allan.course.entities.User;
 import com.allan.course.repositories.UserRepository;
+import com.allan.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
